133909623d0f46eb1a200380003c44fe9b18dfd5
Gurpreet Singh <gurpreet@nvassociated.co>
2025-10-08 14:43:33 +0530

    Navbar logo added

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,11 +1,16 @@
-import React from "react";
-
-function Navbar() {
+const Navbar = () => {
   return (
-    <nav>
-      Hello World
-    </nav>
-  );
+    <>
+      <nav className="mx-auto max-w-[1280px] border border-black px-2" >
+        <div className="w-full h-full flex justify-between">   
+          <div className="max-w-[180px] w-full">
+            <img src="/images/acme-header-logo.png" alt="acme_logo" />
+          </div>
+
+        </div>
+      </nav>
+    </>
+  )
 }
 
-export default Navbar;
+export default Navbar
